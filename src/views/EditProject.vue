92c02427dd36c2f2b06f6d11b2d48298aa303d37
evilimas <eg.vilimas@gmail.com>
2024-03-07 20:00:33 +0100

    init

---
+++ src/views/EditProject.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label for="">Title:</label>
+    <input type="text" required v-model="title" />
+    <label for="">Details:</label>
+    <textarea name="" id="" required v-model="details"></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      title: '',
+      details: '',
+      uri: 'http://localhost:3000/projects/' + this.id,
+    };
+  },
+  mounted() {
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then((data) => {
+        this.title = data.title;
+        this.details = data.details;
+      });
+  },
+  methods: {
+    handleSubmit() {
+      fetch(this.uri, {
+        method: 'PATCH',
+        header: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+          title: this.title,
+          details: this.details,
+        }),
+      })
+        .then(() => {
+          this.$router.push('/');
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style></style>
